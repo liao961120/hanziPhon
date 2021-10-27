334b7cbbc40008552de2142ce269529b043f04ab
liao <liao961120@gmail.com>
2021-10-27 10:22:30 +0800

    finish GuangYun API

--- src/hanziPhon/data/preprocess/廣韻/trim.R
+++ src/hanziPhon/data/preprocess/廣韻/trim.R
@@ -7,9 +7,11 @@ col <- readLines("selected.tsv") %>% strsplit("\t")
 d <- read_csv("廣韻(20170209).csv",
               col_types = cols(.default = col_character()))
 d <- d[, sapply(col, function(x) x[1])]
-colnames(d) <-  sapply(col, function(x) x[3])
+colnames(d) <-  sapply(col, function(x) x[2])
 
-for (c in strsplit("fan_qie pinyin ipa", " ")[[1]] )
+# s = "fan_qie pinyin ipa"
+s = "反切 拼音 IPA"
+for (c in strsplit(s, " ")[[1]] )
     d[[c]] <- strsplit(d[[c]], "/")
 
 writeLines(jsonlite::toJSON(d, pretty=F), "GuangYun.json")
--- src/hanziPhon/guang.py
+++ src/hanziPhon/guang.py
@@ -1,3 +1,5 @@
+#%%
+import re
 import json
 from pathlib import Path
 
@@ -7,29 +9,26 @@ class GuangYun:
     def __init__(self, lexicon:set=None) -> None:
         self._load()
         if lexicon:
-            self.data = [ x for x in self.data if x['chr'] in lexicon ]
-            
+            self.data = [ x for x in self.data if x['字'] in lexicon ]
+
     
-    def find(self, **kwargs):
+    def find(self, return_raw=False, **kwargs):
         """Find hanzi from phonetic features
         """
-        # yun_mu=None, sheng_mu=None, fan_qie=None, pinyin=None, ipa=None, tone=None
-        return [
-            x for x in self.data \
-                if all( v in x[k] if isinstance(x[k], str) else (v in x[k])\
-                        for k, v in kwargs.items())
-        ]
-
-    def match_all(self, data_x, kwargs):
-        """Function to replace all() in self.find()
-        """
-        pass
+        if return_raw:
+            return [ x for x in self.data if match_all(x, kwargs) ]
+        return { x['字'] for x in self.data if match_all(x, kwargs) }
 
 
     def query(self, char:str):
         """Query a hanzi for its phonetic representations
         """
-        return [ x for x in self.data if char == x['chr'] ]
+        return self.find(return_raw=True, 字=char)
+    
+
+    @property
+    def search_params(self):
+        return list(self.data[0].keys())
 
 
     def _load(self):
@@ -38,4 +37,28 @@ class GuangYun:
             self.data = json.load(f)
 
 
-#%%
+
+def match_all(data_x, kwargs):
+    """A filter to compare data and search parameters
+    """
+    mn = 0
+    for k, v in kwargs.items():
+        if k not in data_x:
+            print(f"[WARNING] condition {k} not in data")
+            return False
+        # ipa, pinyin, fan_qie
+        v = re.compile(v)
+        if isinstance(data_x[k], list):
+            for item in data_x[k]:
+                if v.search(item):
+                    mn += 1
+                    break
+        else:
+            if v.search(data_x[k]):
+                mn += 1
+    
+    if mn == len(kwargs):
+        return True
+    return False
+
+# %%
